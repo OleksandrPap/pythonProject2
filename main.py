cc4661389b76cdf7085b7a59395e4a4bcaac7252
AndZPW <100237143+AndZPW@users.noreply.github.com>
2022-10-18 14:59:45 +0300

    update

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask, request
+from flask import Flask
 from waitress import serve
 
 app = Flask(__name__)
+
+
 @app.route('/api/v1/hello-world-18/', methods=['GET'])
 def index():
     return 'Hello World 18'
 
+
 if __name__ == '__main__':
     serve(app, host='127.0.0.1', port=80)
-    #app.run()
+    # app.run()
